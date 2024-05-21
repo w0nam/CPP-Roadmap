68f6aa8813f276be844b5900d8d77d09ac89739d
Lucas <165477518+w0nam@users.noreply.github.com>
2024-05-21 14:46:16 +0200

    Update referencing.cpp

--- simple_programs/referencing.cpp
+++ simple_programs/referencing.cpp
@@ -3,17 +3,17 @@
 
 int main() 
 {
-    int userAge;
-    int& userAgeRef(userAge);
-    std::string userName;
+    int _userAge;
+    int& _userAgeRef(_userAge);
+    std::string _userName;
 
     std::cout << "How old are you ?" << "\n";
-    std::cin >> userAge;
-    std::cout << "You are " << userAge << "yo. (Using userAge)" << "\n";
-    std::cout << "You are " << userAgeRef << "yo. (using userAgeReferences)" << "\n";
+    std::cin >> _userAge;
+    std::cout << "You are " << _userAge << "yo. (Using userAge)" << "\n";
+    std::cout << "You are " << _userAgeRef << "yo. (using userAgeReferences)" << "\n";
     std::cout << "What is your first name ?" << "\n";
-    std::cin >> userName;
-    std::cout << "Your name is " << userName << " & your are " << userAge << "yo." << "\n";
+    std::cin >> _userName;
+    std::cout << "Your name is " << _userName << " & your are " << _userAge << "yo." << "\n";
     std::cout << "-- End of the program. --" << std::endl;
     return 0;
 }
