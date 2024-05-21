ed45755e482fa2069752682411ea163446f40404
Lucas <165477518+w0nam@users.noreply.github.com>
2024-05-21 11:05:46 +0200

    Delete cin_&_cout.cpp

--- cin_&_cout.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main() {
-  int age;
-  std::cout << "how old are you ?" << "\n";
-  std::cin >> age;
-  std::cout << "You are " << age << "yo." << std::endl;
-  return 0;
-}
