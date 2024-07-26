680b8d3368ebda3edb27d5386ce9281dedcad6d5
Lucas <165477518+w0nam@users.noreply.github.com>
2024-07-26 12:48:20 +0200

    Added via upload.
    Basic utilization of 'bool', 'std::cin.ignore()' , 'std::getchar()' & 'if - else' loop.

--- simple_programs/bool.cpp
+++ simple_programs/bool.cpp
@@ -1,21 +1,30 @@
+//Basic utilization of 'bool', 'std::cin.ignore()' && 'if - else' loop.
 #include <iostream>
+#include <cstdio>
 
 int main()
 {
-    bool _adult(true);
-    int _nbrEnfants;
-    
-    std::cout << "how many children do you have ?" << "\n";
-    std::cin >> _nbrEnfants;
+    bool _adult(false);
+    int _age;
 
-    if (_adult && _nbrEnfants >= 1)
-    {
-        std::cout << "You are an adult & you have one or more child." << "\n";
-    }
-    else
-    {
-        std::cout << "You are not an adult OR you don't have any children." << "\n";
-    }
-    
+    std::cout << "\nYou must be 18yo to enter my website. How old are you ?\n\n";
+    std::cin >> _age;
+    std::cin.ignore();
+
+        if (_age >= 18 && _age < 100) {
+            _adult = true;
+            std::cout << "\nLet's ask the computer if you are an adult !\n";
+            std::cout << "is an adult ? " << _adult << " (1 usually means \"yes\" in computer.)" << std::endl;
+            std::cout << "you ARE an adult !" << std::endl;
+        }
+        else {
+            _adult = false;
+            std::cout << "\nLet's adk the computer if you are and adult !\n";
+            std::cout << "is an adult ? " << _adult << std::endl;
+            std::cout << "You are NOT an adult !" << std::endl;
+        }
+
+    std::cout << "\nPress any key to exit..." << std::endl;
+    std::getchar();
     return 0;
 }
