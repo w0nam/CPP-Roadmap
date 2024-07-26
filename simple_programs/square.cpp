195786689208e95afb9460de13a5506f4b569794
Lucas <165477518+w0nam@users.noreply.github.com>
2024-07-26 12:08:41 +0200

    Update square.cpp
    i forgot to input code. fixed !

--- simple_programs/square.cpp
+++ simple_programs/square.cpp
@@ -1,1 +1,19 @@
+//Simple square root calculator using double & cmath lib.
 #include <iostream>
+#include <cmath>
+#include <cstdio>
+
+int main()
+{
+  double _numberA;
+  double _result;
+
+  std::cout << "Enter a value you want to find the square root of: \n";
+  std::cin >> _numberA;
+  std::cin.ignore();
+  _result = sqrt(_numberA);
+  std::cout << "The square root of " << _numberA << " is: " << _result << std::endl;
+  std::cout << "Press any key to exit..." << std::endl;
+  std::getchar();
+  return 0;
+}
