fa49e627684ebce6bcf5ef08fe822300472beb0a
Lucas <165477518+w0nam@users.noreply.github.com>
2024-07-26 12:13:46 +0200

    Update cin_&_cout.cpp
    fixed my code

--- simple_programs/cin_&_cout.cpp
+++ simple_programs/cin_&_cout.cpp
@@ -2,7 +2,7 @@
 
 int main() {
     int _age;
-    std::cout << "How old are you ?" << "\n";
+    std::cout << "How old are you ?\n";
     std::cin >> _age;
     std::cout << "You are " << _age << "yo." << std::endl;
     return 0;
